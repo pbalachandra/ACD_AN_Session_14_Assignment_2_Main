718560947a866724dd3d6643ba7ac93acd71392f
pbalachandra <balachandra.p@gmail.com>
2016-06-08 13:51:11 +0530

    ACD_AN_Session_14_Assignment_2_Main
    ACD_AN_Session_14_Assignment_2_Main

---
+++ app/src/main/java/com/acadgild/balu/acd_an_session_14_assignment_2_main/MainActivity.java
@@ -0,0 +1,106 @@
+package com.acadgild.balu.acd_an_session_14_assignment_2_main;
+
+import android.content.ContentProviderOperation;
+import android.content.ContentResolver;
+import android.content.OperationApplicationException;
+import android.database.Cursor;
+import android.os.RemoteException;
+import android.provider.ContactsContract;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    EditText    editText_name, editText_phone;
+    Button      button_insert;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        editText_name = (EditText) findViewById(R.id.editText_name);
+        editText_phone = (EditText) findViewById(R.id.editText_phone);
+        button_insert = (Button) findViewById(R.id.button_insert);
+
+        button_insert.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v)
+    {
+        if ((editText_name.getText().toString().isEmpty()) ||
+           (editText_phone.getText().toString().isEmpty()))
+        {
+            Toast.makeText(getApplicationContext(), getResources().getString(R.string.empty),
+                    Toast.LENGTH_LONG).show();
+        }
+        else {
+            Insert_Contact(editText_name.getText().toString(), editText_phone.getText().toString());
+        }
+    }
+
+    private void Insert_Contact(String name, String phone)
+    {
+        ContentResolver contentResolver = getContentResolver();
+
+        Cursor cursor = contentResolver.query(ContactsContract.Contacts.CONTENT_URI,
+                                                null, null, null, null);
+        if (cursor.getCount() > 0) {
+            while (cursor.moveToNext()) {
+                String existName = cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME));
+                if (existName.contains(name))
+                {
+                    Toast.makeText(getApplicationContext(),
+                            String.format(getResources().getString(R.string.warning), name),
+                            Toast.LENGTH_LONG).show();
+                    editText_name.setText("");
+                    editText_phone.setText("");
+                    return;
+                }
+            }
+        }
+
+        ArrayList<ContentProviderOperation> contentProvider = new ArrayList<>();
+        contentProvider.add(ContentProviderOperation.newInsert(ContactsContract.RawContacts.CONTENT_URI)
+                .withValue(ContactsContract.RawContacts.ACCOUNT_TYPE, "test@gmail.com")
+                .withValue(ContactsContract.RawContacts.ACCOUNT_NAME, "com.google")
+                .build());
+        contentProvider.add(ContentProviderOperation.newInsert(ContactsContract.Data.CONTENT_URI)
+                .withValueBackReference(ContactsContract.Data.RAW_CONTACT_ID, 0)
+                .withValue(ContactsContract.Data.MIMETYPE,
+                        ContactsContract.CommonDataKinds.StructuredName.CONTENT_ITEM_TYPE)
+                .withValue(ContactsContract.CommonDataKinds.StructuredName.DISPLAY_NAME, name)
+                .build());
+        contentProvider.add(ContentProviderOperation.newInsert(ContactsContract.Data.CONTENT_URI)
+                .withValueBackReference(ContactsContract.Data.RAW_CONTACT_ID, 0)
+                .withValue(ContactsContract.Data.MIMETYPE,
+                        ContactsContract.CommonDataKinds.Phone.CONTENT_ITEM_TYPE)
+                .withValue(ContactsContract.CommonDataKinds.Phone.NUMBER, phone)
+                .withValue(ContactsContract.CommonDataKinds.Phone.TYPE,
+                            ContactsContract.CommonDataKinds.Phone.TYPE_HOME)
+                .build());
+        try {
+            contentResolver.applyBatch(ContactsContract.AUTHORITY, contentProvider);
+        } catch (RemoteException e) {
+
+            e.printStackTrace();
+        } catch (OperationApplicationException e) {
+
+            e.printStackTrace();
+        }
+
+        Toast.makeText(getApplicationContext(), getResources().getString(R.string.message),
+                                                Toast.LENGTH_LONG).show();
+        editText_name.setText("");
+        editText_phone.setText("");
+        cursor.close();
+    }
+}
